b31f94094a6f1f1b18456f8bdb00027871d43ff3
odls <leesj000603@daum.net>
2025-06-02 14:23:31 +0900

    [refactor] #19 - 패키지 대소문자 변경을 위한 임시 이름

--- src/pages/gameroom/GameRoom.jsx
+++ src/pages/gameroom/GameRoom.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import Background from "../../components/Background/Background.jsx";
+import Background from "../../components/Background1/Background.jsx";
 import PlayerList from "./PlayerList";
 import Canvas from "./Canvas";
 import ChatBox from "./ChatBox";
