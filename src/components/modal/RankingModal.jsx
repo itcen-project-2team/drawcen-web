f46c8db0918009187b27dd1fcf4df57d6e1c8e5a
chaewon121 <cw121@naver.com>
2025-06-09 13:41:07 +0900

    [refactor] #21 - 게임 결과 모달창 목데이터 제거

--- src/components/modal/RankingModal.jsx
+++ src/components/modal/RankingModal.jsx
@@ -7,17 +7,9 @@ const RankingModal = ({ isOpen, onClose, rankings, gameId }) => {
 
   // 점수 순으로 정렬
   const sortedRankings = [...rankings].sort((a, b) => b.score - a.score);
-
-  // 임시 테스트용 플레이어 4명 추가
-  const testPlayers = [
-    { memberId: 'test1', nickname: '테스트플레이어테스트 플레이어', score: 85, avatar: null },
-    { memberId: 'test2', nickname: '테스트플레이어2', score: 72, avatar: null },
-    { memberId: 'test3', nickname: '테스트플레이어3', score: 72, avatar: null },
-    { memberId: 'test4', nickname: '테스트플레이어4', score: 68, avatar: null }
-  ];
   
   // 기존 플레이어와 테스트 플레이어를 합쳐서 점수 순으로 정렬
-  const sortedRankingsWithTest = [...sortedRankings, ...testPlayers].sort((a, b) => b.score - a.score);
+  const sortedRankingsWithTest = [...sortedRankings].sort((a, b) => b.score - a.score);
 
   // 동점자 처리를 위한 순위 계산
   const calculateRank = (players) => {
